7f438dd14d3442ab511cc40be0eb4058bf1d6abe
onoyima <clintonfaze@outlook.com>
2023-08-07 10:46:14 -0700

    Commit 0x15 files

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -2,10 +2,10 @@
 #include <stdlib.h>
 
 /**
- * read_textfile- Reads texts file prints to STDOUT.
- * @filenames: texts files being read
- * @letters: numbers of letters to be reads
- * Return: w- actual numbers of bytes reads and printed
+ * read_textfile- Reads text file print to STDOUT.
+ * @filename: text files being read
+ * @letters: number of letters to be read
+ * Return: w- actual numbers of bytes read and printed
  *        0 when function fails or filename is NULL.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
