f85e76e44d4e0f3d86c54e1fb4ab2d7b4bc2d456
onoyima <clintonfaze@outlook.com>
2023-10-30 15:49:23 -0700

    Committing 0x15-file

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - Append texts at the end of a files.
+ * @filename: A pointers to the names of the files.
+ * @text_content: The string to adds to the end of the files.
+ *
+ * Return: If the functions fails or filenames is NULL - -1.
+ *         If the files does not exist the user lacks writes permissions - -1.
+ *         Otherwise - 1.
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+
+	int o, w, len = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
+
+	o = open(filename, O_WRONLY | O_APPEND);
+	w = write(o, text_content, len);
+
+	if (o == -1 || w == -1)
+		return (-1);
+
+	close(o);
+
+	return (1);
+}
